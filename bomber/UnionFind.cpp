60a0dbd17dc536edfa5ba131008ee5996be8653d
Irene Song <irenesong@csilvm-09.cs.ucsb.edu>
2025-03-18 16:30:06 -0700

    edited unionfind.cpp

---
+++ bomber/UnionFind.cpp
@@ -0,0 +1,39 @@
+#include "UnionFind.h"
+
+UnionFind::UnionFind()
+{
+}
+
+void UnionFind::insert(int x, Point p)
+{
+    points[x] = p;
+    parents.push_back(x);
+    visited.push_back(false);
+}
+
+int UnionFind::find(int x)
+{
+    if(parents[x] != x)
+        parents[x] = find(parents[x]);
+    return parents[x];
+}
+
+void UnionFind::unite(int a, int b)
+{
+    int aRoot = find(a);
+    int bRoot = find(b);
+    if(aRoot!=bRoot)
+        parents[aRoot] = bRoot; //doesn't matter which one becomes root (?)
+    return;
+}
+void UnionFind::connectPoints()
+{
+    if(points.size()==0)
+        return;
+    Point current = points[0];
+}
+
+//add new var to count ins
+//add after line 45 in Map.cpp
+//(UnionFindObjName).insert(ins,Point(y,x))
+//increment ins after every insert
