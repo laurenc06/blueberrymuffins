5c336a26d0ccfa332dd6853db7a42e515b0727b1
Lauren Cho <laurencho@ucsb.edu>
2025-03-19 14:07:55 -0700

    Updated Node.cpp

--- bomber/UnionFind.h
+++ bomber/UnionFind.h
@@ -12,20 +12,20 @@ using namespace std;
 class UnionFind {
     vector<int> parents;
     vector<bool> visited;
-    map<int,Map::Node> points;
+    map<int,Node> points;
     int cols; // # of columns
     int rows; // # of rows
 
     public: 
         UnionFind();
         void setCols(int c);
-        void insert(int x, Map::Node point);
+        void insert(int x, Node point);
         int find(int x);
-        int findIndex(Map::Node current);
+        int findIndex(Node current);
         void unite(int a, int b);
         void connectPoints(int index);
         void connectAll();
-        bool shouldBomb(Map::Node current, Map::Node neighbor, Map::Node end);
+        bool shouldBomb(Node current, Node neighbor, Node end);
 };
 
 #endif
