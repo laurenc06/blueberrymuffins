244dd780becec7dc953d31b5f7628fd13b853001
Irene Song <irenesong@csilvm-09.cs.ucsb.edu>
2025-03-18 21:58:38 -0700

    edited unionfind.h

--- bomber/UnionFind.h
+++ bomber/UnionFind.h
@@ -9,13 +9,15 @@ class UnionFind {
     vector<int> parents;
     vector<bool> visited;
     map<int,Map::Node> points;
-    int cols; //# of columns
+    int cols; // # of columns
+    int rows; // # of rows
 
     public: 
         UnionFind();
         void setCols(int c);
         void insert(int x, Map::Node point);
         int find(int x);
+        const int findIndex(const Map::Node x);
         void unite(int a, int b);
         void connectPoints(int index);
         void connectAll();
