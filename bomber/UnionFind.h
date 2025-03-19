39ab000eb11f2ed303450434519efa653295ed0c
Irene Song <irenesong@csilvm-09.cs.ucsb.edu>
2025-03-19 14:01:57 -0700

    trying to fix errors

--- bomber/UnionFind.h
+++ bomber/UnionFind.h
@@ -1,6 +1,6 @@
 #include <vector>
 #include <map>
-#include "Map.h"
+#include "Node.h"
 #include "Point.h"
 
 using namespace std;
@@ -17,10 +17,9 @@ class UnionFind {
         void setCols(int c);
         void insert(int x, Map::Node point);
         int find(int x);
-        const int findIndex(const Map::Node x);
+        int findIndex(Map::Node current);
         void unite(int a, int b);
         void connectPoints(int index);
         void connectAll();
-
         bool shouldBomb(Map::Node current, Map::Node neighbor, Map::Node end);
 };
